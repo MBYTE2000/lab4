ba0be287c0a34e8cb6000e195509a86f81782e4b
MBYTE2000 <astriyonok@mail.ru>
2022-12-01 22:39:43 +0300

    123

---
+++ lab4.1/lab4.1.cpp
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+
+#include <conio.h>
+
+#include <math.h>
+
+#pragma hdrstop
+
+//---------------------------------------------------------------------------
+
+#pragma argsused
+
+void Out_Rez(double, double, double, double);
+
+int main(int argc, char* argv[])
+
+{
+
+    double a, b, x, h, y, s;
+
+    int n, k, fact, r;
+
+    puts("Input a,b,h,n");
+
+    scanf_s("%f%f%f%d", &a, &b, &h, &n);
+
+    for (x = a; x <= b; x += h) {
+
+        y = exp(2 * x);
+
+        fact = 1; s = 0;
+
+        for (k = 0; k <= n; k++) {
+
+            if (k == 0) fact = 1;
+
+            else fact = k;
+
+            s += pow(2*x, k) / fact;
+
+        }
+
+        r = fabs(y - s);
+
+        Out_Rez(x, y, s, r);
+
+    }
+
+    return 0;
+}
+
+void Out_Rez(double x, double y, double s, double r)
+
+{
+
+    printf("\n x= %8.2f", x);
+
+    printf("\n Y(x)= %8.6f", y);
+
+    printf("\n S(x)= %8.6f", s);
+
+    printf("\n |Y(X)-S(X)|= %8.6f", r);
+
+}
--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -5,56 +5,79 @@ using namespace std;
 
 
 typedef double (*TFun)(double);
+typedef double (*SFun)(double, int);
 double fun1(double);			
 double fun2(double);
-void OutRez(TFun f, double x, double b, double h, int n);
+double S1(double r, int n);
+double S2(double r, int n);
+void OutRez(TFun F, SFun S, double x, double b, double h, int n);
 
+const double PI = 3.14;
+
+//-------------------------------------------------
 int main() {
 	setlocale(LC_ALL, "ru");
 	double a=0.1, b=1, h=0.1;
-	int n = 10;
+	int n = 100;
 
 
 	puts("Введите a,b,h,n");
 	//cin >> a, b, h, n;
 	cout << "\n\t Function - 2*pow(cos(r-1), 2)\n";
-	OutRez(fun1, a, b, h, n);
+	OutRez(fun1, S1, a, b, h, n);
 	cout << "\n\t Function - pow(sin(r), 2)\n";
-	OutRez(fun2, a, b, h, n);
+	OutRez(fun2, S2, a, b, h, n);
 	return 0;
 }
 
-
+//----------------------------------------------------------------
 double fun1(double r) {
-	return 2*pow(cos(r-1), 2);
+	//return 2*pow(cos(r-1), 2);
+	return (r*r/4 + r/2 +1)*exp(r/2);
 }
 double fun2(double r) {
-	return pow(sin(r), 2);
+	return exp(r * cos(PI / 4.)) * cos(r * sin(PI / 4.));
 }
 
 
-void OutRez(TFun f, double x, double b, double h, int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << "x=" << i << " y=" << f(x) << endl;
-	}
 
-	/*double p, s, y, m, fa;
+double S1(double r, int n) {
+	double s = 1, fa=1, p=1;
+	/*for (int i = 1; i <= n; i++)
+	{
+		fa *= i*(i+1);
+		p *= -((2 * r) * (2 * r))/fa;
+		s += p;
+	}*/
+
+	for (int i = 1; i <= n; i++)
+	{
+		p *= (r / 2) / i;
+		s += (i * i + 1)*p;
+	}
+	return s;
+}
+double S2(double r, int n) {
+	double s = 1, p = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		p *= r / i;
+		s += cos(i*PI / 4) * p;
+	}
+	return s;
+}
+
+
+void OutRez(TFun F, SFun S, double x, double b, double h, int n)
+{
+	double p, s, y, m, fa;
 	do
 	{
-		p = 1;
-		s = fa = 0;
-		for (int i = 1; i <= n; i++)
-		{
-			p *= -((2*x)*(2*x));
-			fa += 2*i;
-			s += p / fa;
-		}
-		y = f(x);
+		s = S(x, n);
+		y = F(x);
 		m = abs(y - s);
 		cout << setw(15) << x << setw(15) << y << setw(15) << s << setw(15) << m << endl;
 		x += h;
-	} while (x <= b + h / 2.);*/
+	} while (x <= b + h / 2.);
+	
 }
-
